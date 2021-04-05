00527bbdd68f58bfb65b16c100ee824151a3f708
anirudhtomer_p95 <anirudh.tomer@p-95.com>
2021-04-05 13:17:04 +0200

    precision code generalized for both cohort and case-control. also tested that it is unbiased

--- sample_size/casecontrol_precision_VE.R
+++ sample_size/casecontrol_precision_VE.R
@@ -4,16 +4,15 @@ source("sample_size/util.R")
 #In general P(Case) is really low for diseases like influenza or even dengue. hardly 1-3% in the population
 #Hence overall_vaccine_coverage is almost equal to vaccine coverage in controls
 
-#all parameters are scalars except anticipated_VE_othervaccines
-#coverage: coverage in overall population
+#all parameters are scalars except anticipated_brand_VEs and overall_brand_proportions
 #nsims: larger value give more accurate results
 #Important
 #we have a n x 2 table for this situation, with n-1 rows for n-1 vaccines and 1 row for unvaccinated people
 #the 2 columns are for cases and controls. our aim is to obtain a cell count for each cell
 #there is a controls:case ratio which is selected on a population level and not on vaccine level
 
-casecontrol_precision_VE = function(anticipated_brand_VEs=c(0.8, 0.5, 0.7),
-                                    overall_brand_proportions = c(30, 50, 20),
+casecontrol_precision_VE = function(anticipated_brand_VEs=c(0.8, 0.5, 0.3),
+                                    overall_brand_proportions = c(0.3, 0.5, 0.2),
                                     overall_vaccine_coverage=0.3, 
                                     attack_rate_unvaccinated = 0.1,
                                     alpha=0.05, 
@@ -37,6 +36,7 @@ casecontrol_precision_VE = function(anticipated_brand_VEs=c(0.8, 0.5, 0.7),
   total_controls = missing_data_adjusted_total_cases * controls_per_case
   
   brand_vaccine_coverages = overall_brand_proportions * overall_vaccine_coverage
+  #assuming that coverage in controls is same as overall coverage (see top of this file)
   cell_prob_control = c(brand_vaccine_coverages, 1-overall_vaccine_coverage)
   
   upp = matrix(ncol=total_vaccines, nrow = nsims, data = NA)
@@ -63,7 +63,7 @@ casecontrol_precision_VE = function(anticipated_brand_VEs=c(0.8, 0.5, 0.7),
   avg_upper_limit_VE = apply(1-low, MARGIN = 2, FUN = mean, na.rm=T)
   
   prob_unvaccinated_case = attack_rate_unvaccinated * (1 - overall_vaccine_coverage)
-  prob_vaccinated_case = vaccine_coverages * (1 + (1-attack_rate_unvaccinated)/((1-anticipated_brand_VEs) * attack_rate_unvaccinated))^-1
+  prob_vaccinated_case = brand_vaccine_coverages * (1 + (1-attack_rate_unvaccinated)/((1-anticipated_brand_VEs) * attack_rate_unvaccinated))^-1
   prob_case = sum(prob_unvaccinated_case, prob_vaccinated_case)
   
   catchment = total_cases / (prob_case *prob_getting_swabbed_given_ili_sari * ili_sari_symptom_prob)
--- sample_size/cohort_precision_VE.R
+++ sample_size/cohort_precision_VE.R
@@ -3,10 +3,11 @@ source("sample_size/util.R")
 #all parameters are scalars
 #coverage: coverage in overall population
 #nsims: larger value give more accurate results
-cohort_precision_VE = function(anticipated_VE=0.8, alpha=0.05, 
-                               brand_proportion=0.8,
+cohort_precision_VE = function(anticipated_brand_VEs=c(0.8, 0.5, 0.3),
+                               overall_brand_proportions = c(0.3, 0.5, 0.2),
                                overall_vaccine_coverage=0.3, 
-                               attack_rate_unvaccinated=0.5,
+                               attack_rate_unvaccinated = 0.1,
+                               alpha=0.05, 
                                confounder_adjustment_Rsquared = 0,
                                prob_missing_data = 0.1, 
                                prob_getting_swabbed_given_ili_sari = 0.5,
@@ -19,41 +20,52 @@ cohort_precision_VE = function(anticipated_VE=0.8, alpha=0.05,
   #I do it first because it checks if default NA parameters have been given required value
   ili_sari_symptom_prob = get_ili_sari_symptom_prob(ili_sari_symptom_prob, ili_sari_symptom_incidence_rate, study_period_length)
   
-  brand_coverage = brand_proportion *overall_vaccine_coverage
-  brand_and_unvaccinated_coverage = brand_coverage + 1-overall_vaccine_coverage
+  if(!sum(overall_brand_proportions, na.rm = T)==1){
+    stop("Sum of brand proportions should be equal to 1")
+  }
   
-  total_subjects = total_subjects * brand_and_unvaccinated_coverage
+  total_vaccines = length(anticipated_brand_VEs)
+  brand_vaccine_coverages = overall_brand_proportions * overall_vaccine_coverage
   missing_data_adjusted_total_subjects = round(total_subjects * (1-prob_missing_data))
   
-  overall_vaccine_coverage=brand_coverage / brand_and_unvaccinated_coverage
+  #first n-1 numbers are 
+  relative_risks = c(1 - anticipated_brand_VEs)
+  prob_vaccinated_case = attack_rate_unvaccinated * brand_vaccine_coverages * relative_risks
+  #made a dummy variable for ease of reading the code and the math
+  relative_risk_placebo = 1
+  prob_unvaccinated_case = attack_rate_unvaccinated * (1-overall_vaccine_coverage) * relative_risk_placebo
+  prob_vaccinated_control = brand_vaccine_coverages * (1- attack_rate_unvaccinated*relative_risks)
+  prob_unvaccinated_control = (1-attack_rate_unvaccinated) * (1-overall_vaccine_coverage)
   
-  relative_risk = 1 - anticipated_VE
-  p_a_vaccinated_case = attack_rate_unvaccinated * overall_vaccine_coverage * relative_risk
-  p_b_vaccinated_control = overall_vaccine_coverage * (1- attack_rate_unvaccinated*relative_risk)
-  p_c_unvaccinated_case = attack_rate_unvaccinated * (1-overall_vaccine_coverage)
-  p_d_unvaccinated_control = (1-attack_rate_unvaccinated) * (1-overall_vaccine_coverage)
+  upp = matrix(ncol=total_vaccines, nrow = nsims, data = NA)
+  low = matrix(ncol=total_vaccines, nrow = nsims, data = NA)
   
-  upp = rep(NA, nsims)
-  low = rep(NA, nsims)
-  
+  CASE=1
+  CONTROL=2
   for(j in 1:nsims){
-    temp = c(rmultinom(n=1, size=missing_data_adjusted_total_subjects, prob = c(p_a_vaccinated_case, p_b_vaccinated_control, p_c_unvaccinated_case, p_d_unvaccinated_control)))
+    cell_counts = c(rmultinom(n=1, size=missing_data_adjusted_total_subjects, 
+                              prob = c(prob_vaccinated_case, prob_unvaccinated_case, 
+                                       prob_vaccinated_control, prob_unvaccinated_control)))
+    #2 x n table, row 1 for cases, row 2 for controls. nth column for unvaccinated and other columns for different vaccines
+    cell_counts = matrix(cell_counts, nrow=2, byrow = T)
     
-    a_vaccinated_case = temp[1]
-    b_vaccinated_control = temp[2]
-    c_unvaccinated_case = temp[3]
-    d_unvaccinated_control = temp[4]
+    estimates = (cell_counts[CASE, 1:total_vaccines]/colSums(cell_counts[, 1:total_vaccines])) / (cell_counts[CASE, total_vaccines+1]/sum(cell_counts[, total_vaccines+1]))
+    standard_errors = sqrt(
+      cell_counts[CASE, 1:total_vaccines]^-1 + 
+        cell_counts[CASE, total_vaccines+1]^-1 - 
+        (cell_counts[CASE, 1:total_vaccines] + cell_counts[CONTROL, 1:total_vaccines])^-1 - 
+        (cell_counts[CASE, total_vaccines+1] + cell_counts[CONTROL, total_vaccines+1])^-1
+    )
     
-    estimate = (a_vaccinated_case/(a_vaccinated_case + b_vaccinated_control)) / (c_unvaccinated_case/(c_unvaccinated_case + d_unvaccinated_control))
-    se = sqrt(1/a_vaccinated_case + 1/c_unvaccinated_case - 1/(a_vaccinated_case + b_vaccinated_control) - 1/(c_unvaccinated_case + d_unvaccinated_control))
-    confounder_adjusted_se = se / (1-confounder_adjustment_Rsquared)
-    low = exp(log(estimate) + qnorm(alpha/2) * confounder_adjusted_se)
-    upp = exp(log(estimate) + qnorm(1-alpha/2) * confounder_adjusted_se)
+    confounder_adjusted_standard_errors = standard_errors / (1-confounder_adjustment_Rsquared)
+    low[j,] = exp(log(estimates) + qnorm(alpha/2) * confounder_adjusted_standard_errors)
+    upp[j,] = exp(log(estimates) + qnorm(1-alpha/2) * confounder_adjusted_standard_errors)
   }
+  
+  avg_lower_limit_VE = apply(1-upp, MARGIN = 2, FUN = mean, na.rm=T)
+  avg_upper_limit_VE = apply(1-low, MARGIN = 2, FUN = mean, na.rm=T)
   
   catchment = total_subjects / (prob_getting_swabbed_given_ili_sari * ili_sari_symptom_prob)
   
-  return(c(avg_lower_limit_VE = mean(1-upp, na.rm = T), 
-           avg_upper_limit_VE = mean(1-low, na.rm = T),
-           catchment=catchment))
+  return(list(avg_lower_limit_VE=avg_lower_limit_VE, avg_upper_limit_VE=avg_upper_limit_VE, catchment=catchment))
 }
