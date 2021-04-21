376237ab2e670138f6e1a5c7e7d5c81a6a99ea6f
anirudhtomer_p95 <anirudh.tomer@p-95.com>
2021-04-21 16:04:45 +0200

    all operations done over a 3D matrix, added strain specific calculations'

--- R/get_casecontrol_expectedCI_VE.R
+++ R/get_casecontrol_expectedCI_VE.R
@@ -1,87 +1,124 @@
 #' @importFrom utils combn
 #' @export
-get_casecontrol_expectedCI_VE = function(anticipated_brand_VEs=c(0.8, 0.5, 0.3),
-                                         overall_brand_proportions = c(0.3, 0.5, 0.2),
+get_casecontrol_expectedCI_VE = function(anticipated_VE_for_each_brand_and_strain=
+                                           list('brand1'=c('strain1'=0.8, 'strain2'=0.3, 'strain3'=0.9),
+                                                'brand2'=c('strain1'=0.5, 'strain2'=0.5, 'strain3'=0.5),
+                                                'brand3'=c('strain1'=0.3, 'strain2'=0.8, 'strain3'=0.1)),
+                                         brand_proportions_in_vaccinated =
+                                           c('brand1'=0.3, 'brand2'=0.5, 'brand3'=0.2),
                                          overall_vaccine_coverage=0.3,
+                                         proportion_strains_in_unvaccinated_cases = c('strain1'=0.6, 'strain2'=0.3, 'strain3'=0.1),
                                          controls_per_case=2,
-                                         calculate_for_relative_VE = T,
+                                         calculate_relative_VE = T,
                                          alpha=0.05,
                                          confounder_adjustment_Rsquared = 0,
                                          prob_missing_data = 0.1,
-                                         total_cases=500,
+                                         total_cases=seq(50,500, 10),
                                          nsims = 500){
-  #first something important regarding vaccine coverage in controls
+
+  if(!sum(brand_proportions_in_vaccinated, na.rm = T)==1){
+    stop("Sum of the values of the parameter 'brand_proportions_in_vaccinated' should be equal to 1")
+  }
+
+  if(!sum(proportion_strains_in_unvaccinated_cases, na.rm = T)==1){
+    stop("Sum of the values of the parameter 'proportion_strains_in_unvaccinated_cases' should be equal to 1")
+  }
+
+  if(length(anticipated_VE_for_each_brand_and_strain)!=length(brand_proportions_in_vaccinated)){
+    stop("Length of the parameter 'anticipated_VE_for_each_brand_and_strain' should be equal to length of the parameter 'brand_proportions_in_vaccinated'")
+  }
+
+  total_total_case_settings = length(total_cases)
+
+  missing_data_adjusted_total_cases = round(total_cases * (1-prob_missing_data))
+  missing_data_adjusted_total_controls = missing_data_adjusted_total_cases * controls_per_case
+
+  #For each vaccine we know in what proportion are they given in the general population
+  total_vaccine_brands = length(brand_proportions_in_vaccinated)
+  #For each strain we know in what proportion are they affect the total number of cases
+  total_case_strains = length(proportion_strains_in_unvaccinated_cases)
+
+  relative_VE_combn = matrix(c(1:total_vaccine_brands, rep(total_vaccine_brands+1, total_vaccine_brands)), byrow = T, nrow = 2)
+  if(calculate_relative_VE & total_vaccine_brands>1){
+    relative_VE_combn = cbind(combn(total_vaccine_brands, 2), relative_VE_combn)
+  }
+  relative_VE_combn = rbind(relative_VE_combn[,rep(1:ncol(relative_VE_combn), each=total_case_strains)], rep(1:total_case_strains, total_case_strains))
+
+  #We are going to create a table with dimensions (row x columns) = (total_vaccine_brands + 1) x (total_case_strains + 1)
+  #Note that this table is the transpose of what we did in the cohort study.
+  #The extra 1 column is for unvaccinated
+  #The extra 1 row is for controls
+
+  prob_vaccinated_each_brand = brand_proportions_in_vaccinated * overall_vaccine_coverage
   #P(Vaccination) = P(Vaccination | Case) P(Case) + P(Vaccination | Control) (1-P(Case))
   #In general P(Case) is really low for diseases like influenza or even dengue. hardly 1-3% in the population
   #Hence overall_vaccine_coverage is almost equal to vaccine coverage in controls
+  prob_vaccinated_each_brand_given_control = prob_vaccinated_each_brand
+  prob_unvaccinated_given_control = 1 - overall_vaccine_coverage
 
-  #all parameters are scalars except anticipated_brand_VEs and overall_brand_proportions
-  #nsims: larger value give more accurate results
-  #Important
-  #we have a n x 2 table for this situation, with n-1 rows for n-1 vaccines and 1 row for unvaccinated people
-  #the 2 columns are for cases and controls. our aim is to obtain a cell count for each cell
-  #there is a controls:case ratio which is selected on a population level and not on vaccine level
+  STRAIN_ROW = 3
+  UNVACCINATED_ROW = total_vaccine_brands+1
+  #the idea in a case-control study is that we generate two sets of multinomial data
+  #1. for controls
+  #2. for cases
+  #the counts remain constant
 
-  if(!sum(overall_brand_proportions, na.rm = T)==1){
-    stop("Sum of brand proportions should be equal to 1")
-  }else if(length(anticipated_brand_VEs)!=length(overall_brand_proportions)){
-    stop("Length of anticipated brand VE should be equal to length of overall brand proportions")
-  }
+  odds_vaccinated_for_each_brand_and_strain = 1 - do.call('rbind', anticipated_VE_for_each_brand_and_strain)
+  prob_case_and_unvaccinated = sum(proportion_strains_in_unvaccinated_cases * (1 + colSums(odds_vaccinated_for_each_brand_and_strain * prob_vaccinated_each_brand_given_control)/prob_unvaccinated_given_control))^-1
+  prob_unvaccinated_case_each_strain = prob_case_and_unvaccinated * proportion_strains_in_unvaccinated_cases
+  prob_vaccinated_case_each_strain_and_brand = t(t(odds_vaccinated_for_each_brand_and_strain * prob_vaccinated_each_brand_given_control / prob_unvaccinated_given_control) * prob_unvaccinated_case_each_strain)
 
-  total_vaccines = length(anticipated_brand_VEs)
+  full_table_cases = rbind(prob_vaccinated_case_each_strain_and_brand, prob_unvaccinated_case_each_strain)
 
-  relative_VE_combn = matrix(c(1:total_vaccines, rep(total_vaccines+1, total_vaccines)), byrow = T, nrow = 2)
-  if(calculate_for_relative_VE & total_vaccines>1){
-    relative_VE_combn = cbind(combn(total_vaccines, 2), relative_VE_combn)
-  }
+  ##c(full_table_cases) converts the table into a vector, going column by column
+  cell_counts_case_sims = array(data = do.call('cbind', lapply(missing_data_adjusted_total_cases, rmultinom, n=nsims, prob=c(full_table_cases))),
+                                dim = c((total_vaccine_brands+1)*total_case_strains, nsims, total_total_case_settings))
+  #Haldane's correction
+  cell_counts_case_sims[cell_counts_case_sims==0] = 0.5
 
-  missing_data_adjusted_total_cases = round(total_cases * (1-prob_missing_data))
-  total_controls = missing_data_adjusted_total_cases * controls_per_case
+  #cell_count_control_sims has dimension total_vaccine_brands x nsims
+  cell_counts_control_sims = array(data = do.call('cbind', lapply(missing_data_adjusted_total_controls, rmultinom, n=nsims, prob=c(prob_vaccinated_each_brand_given_control, prob_unvaccinated_given_control))),
+                                   dim = c(total_vaccine_brands+1, nsims, total_total_case_settings))
+  #Haldane's correction
+  cell_counts_control_sims[cell_counts_control_sims==0] = 0.5
 
-  brand_vaccine_coverages = overall_brand_proportions * overall_vaccine_coverage
-  #assuming that coverage in controls is same as overall coverage (see top of this file)
-  cell_prob_control = c(brand_vaccine_coverages, 1-overall_vaccine_coverage)
+  #dimensions of the following are ncol(relative_VE_combn) x nsims
+  cases_vaccine1 = cell_counts_case_sims[(relative_VE_combn[STRAIN_ROW,]-1)*(total_vaccine_brands+1) + relative_VE_combn[1,],,,drop=F]
+  cases_vaccine2 = cell_counts_case_sims[(relative_VE_combn[STRAIN_ROW,]-1)*(total_vaccine_brands+1) + relative_VE_combn[2,],,,drop=F]
+  controls_vaccine1 = cell_counts_control_sims[relative_VE_combn[1,],,,drop=F]
+  controls_vaccine2 = cell_counts_control_sims[relative_VE_combn[2,],,,drop=F]
 
-  estimate = matrix(ncol=ncol(relative_VE_combn), nrow = nsims, data = NA)
-  upp = matrix(ncol=ncol(relative_VE_combn), nrow = nsims, data = NA)
-  low = matrix(ncol=ncol(relative_VE_combn), nrow = nsims, data = NA)
+  estimate = (cases_vaccine1/cases_vaccine2) / (controls_vaccine1/controls_vaccine2)
+  standard_error = sqrt(1/cases_vaccine1 + 1/cases_vaccine2 + 1/controls_vaccine1 + 1/controls_vaccine2)
+  confounder_adjusted_standard_error = standard_error / (1-confounder_adjustment_Rsquared)
 
-  for(j in 1:nsims){
+  log_estimate = log(estimate)
+  quantile_times_adjusted_standard_error = qnorm(1-alpha/2) * confounder_adjusted_standard_error
+  low = exp(log_estimate - quantile_times_adjusted_standard_error)
+  upp = exp(log_estimate + quantile_times_adjusted_standard_error)
 
-    #first n-1 cells are of different vaccines, the n-th cell is of unvaccinated people
-    cell_count_controls = c(rmultinom(n = 1, size = total_controls, prob = cell_prob_control))
-    #Haldane's correction
-    cell_count_controls[cell_count_controls==0] = 0.5
+  #expected_VE = rowMeans(1-estimate, na.rm=T)
+  expected_VE = apply(X = 1-estimate, MARGIN = 3, FUN = rowMeans, na.rm=T)
+  avg_lower_limit = apply(X = 1-upp, MARGIN = 3, FUN = rowMeans, na.rm=T)
+  avg_upper_limit = apply(X = 1-low, MARGIN = 3, FUN = rowMeans, na.rm=T)
 
-    cell_prob_unvaccinated_case = (1 + sum(cell_count_controls[1:total_vaccines] * (1-anticipated_brand_VEs))/cell_count_controls[total_vaccines+1])^-1
-    cell_prob_vaccinated_cases = (1-anticipated_brand_VEs) * cell_count_controls[1:total_vaccines] * cell_prob_unvaccinated_case / cell_count_controls[total_vaccines+1]
-    cell_count_cases = c(rmultinom(n = 1, size = missing_data_adjusted_total_cases, prob = c(cell_prob_vaccinated_cases, cell_prob_unvaccinated_case)))
-    #Haldane's correction
-    cell_count_cases[cell_count_cases==0] = 0.5
+  odds_vaccinated_for_each_brand_and_strain = rbind(odds_vaccinated_for_each_brand_and_strain,1)
+  odds_vaccinated_vaccine1 = odds_vaccinated_for_each_brand_and_strain[(relative_VE_combn[STRAIN_ROW,]-1)*(total_vaccine_brands+1) + relative_VE_combn[1,]]
+  odds_vaccinated_vaccine2 = odds_vaccinated_for_each_brand_and_strain[(relative_VE_combn[STRAIN_ROW,]-1)*(total_vaccine_brands+1) + relative_VE_combn[2,]]
+  anticipated_VE = 1 - odds_vaccinated_vaccine1/odds_vaccinated_vaccine2
 
-    for(k in 1:ncol(relative_VE_combn)){
-      v1index = relative_VE_combn[1, k]
-      v2index = relative_VE_combn[2, k]
+  ret = data.frame(vaccine_1 = rep(paste("Vaccine", relative_VE_combn[1,]),total_total_case_settings),
+                   vaccine_2 = rep(ifelse(relative_VE_combn[2,]==total_vaccine_brands+1,
+                                          no = paste("Vaccine", relative_VE_combn[2,]),
+                                          yes = "Unvaccinated"), total_total_case_settings),
+                   strain = rep(paste("Strain", relative_VE_combn[STRAIN_ROW,]), total_total_case_settings),
+                   total_cases = rep(total_cases, each=ncol(relative_VE_combn)),
+                   controls_per_case = controls_per_case,
+                   anticipated_VE = anticipated_VE,
+                   expected_VE = c(expected_VE),
+                   bias_VE = c(expected_VE - anticipated_VE),
+                   avg_lower_limit = c(avg_lower_limit),
+                   avg_upper_limit = c(avg_upper_limit))
 
-      estimate[j,k] = (cell_count_cases[v1index]/cell_count_cases[v2index]) / (cell_count_controls[v1index]/cell_count_controls[v2index])
-      standard_error = sqrt(cell_count_cases[v1index]^-1 + cell_count_cases[v2index]^-1 + cell_count_controls[v1index]^-1 + cell_count_controls[v2index]^-1)
-      confounder_adjusted_standard_error = standard_error / (1-confounder_adjustment_Rsquared)
-      low[j,k] = exp(log(estimate[j,k]) + qnorm(alpha/2) * confounder_adjusted_standard_error)
-      upp[j,k] = exp(log(estimate[j,k]) + qnorm(1 - alpha/2) * confounder_adjusted_standard_error)
-    }
-  }
-
-  anticipated_brand_VEs = c(anticipated_brand_VEs, 0)
-
-  ret = data.frame(Vaccine1=paste("Vaccine", relative_VE_combn[1,]),
-                   Vaccine2=ifelse(relative_VE_combn[2,]==total_vaccines+1, no = paste("Vaccine", relative_VE_combn[2,]), yes = "Unvaccinated"),
-                   anticipated_VE = apply(relative_VE_combn, 2, FUN = function(x){
-                     1 - (1-anticipated_brand_VEs[x[1]])/(1-anticipated_brand_VEs[x[2]])
-                   }),
-                   expected_VE = apply(1-estimate, MARGIN = 2, FUN = mean, na.rm=T),
-                   avg_lower_limit = apply(1-upp, MARGIN = 2, FUN = mean, na.rm=T),
-                   avg_upper_limit = apply(1-low, MARGIN = 2, FUN = mean, na.rm=T))
-
-  ret$bias_VE = ret$expected_VE - ret$anticipated_VE
   return(ret)
 }
--- R/get_cohort_expectedCI_VE.R
+++ R/get_cohort_expectedCI_VE.R
@@ -1,84 +1,128 @@
 #' @importFrom utils combn
 #' @export
-get_cohort_expectedCI_VE = function(anticipated_brand_VEs=c(0.8, 0.5, 0.3),
-                                    overall_brand_proportions = c(0.3, 0.5, 0.2),
+get_cohort_expectedCI_VE = function(anticipated_VE_for_each_brand_and_strain=
+                                      list('brand1'=c('strain1'=0.8, 'strain2'=0.3, 'strain3'=0.9),
+                                           'brand2'=c('strain1'=0.5, 'strain2'=0.5, 'strain3'=0.5),
+                                           'brand3'=c('strain1'=0.3, 'strain2'=0.8, 'strain3'=0.1)),
+                                    brand_proportions_in_vaccinated =
+                                      c('brand1'=0.3, 'brand2'=0.5, 'brand3'=0.2),
                                     overall_vaccine_coverage=0.3,
-                                    attack_rate_unvaccinated = 0.1,
-                                    calculate_for_relative_VE = T,
+                                    proportion_strains_in_unvaccinated_cases = c('strain1'=0.6, 'strain2'=0.3, 'strain3'=0.1),
+                                    overall_attack_rate_in_unvaccinated = 0.1,
+                                    calculate_relative_VE = T,
                                     alpha=0.05,
                                     confounder_adjustment_Rsquared = 0,
                                     prob_missing_data = 0.1,
-                                    total_subjects=500,
+                                    total_subjects=seq(1000, 10000, 25),
                                     nsims = 500){
 
-  if(!sum(overall_brand_proportions, na.rm = T)==1){
-    stop("Sum of brand proportions should be equal to 1")
-  }else if(length(anticipated_brand_VEs)!=length(overall_brand_proportions)){
-    stop("Length of anticipated brand VE should be equal to length of overall brand proportions")
+  if(!sum(brand_proportions_in_vaccinated, na.rm = T)==1){
+    stop("Sum of the values of the parameter 'brand_proportions_in_vaccinated' should be equal to 1")
   }
 
-  total_vaccines = length(anticipated_brand_VEs)
-
-  relative_VE_combn = matrix(c(1:total_vaccines, rep(total_vaccines+1, total_vaccines)), byrow = T, nrow = 2)
-  if(calculate_for_relative_VE & total_vaccines>1){
-    relative_VE_combn = cbind(combn(total_vaccines, 2), relative_VE_combn)
+  if(!sum(proportion_strains_in_unvaccinated_cases, na.rm = T)==1){
+    stop("Sum of the values of the parameter 'proportion_strains_in_unvaccinated_cases' should be equal to 1")
   }
 
-  brand_vaccine_coverages = overall_brand_proportions * overall_vaccine_coverage
+  if(length(anticipated_VE_for_each_brand_and_strain)!=length(brand_proportions_in_vaccinated)){
+    stop("Length of the parameter 'anticipated_VE_for_each_brand_and_strain' should be equal to length of the parameter 'brand_proportions_in_vaccinated'")
+  }
+
+  total_total_subject_settings = length(total_subjects)
+
+  #Assuming that the all subjects have the same chance of dropout due to one or another reason
   missing_data_adjusted_total_subjects = round(total_subjects * (1-prob_missing_data))
 
-  #first n-1 numbers are
-  relative_risks = c(1 - anticipated_brand_VEs)
-  prob_vaccinated_case = attack_rate_unvaccinated * brand_vaccine_coverages * relative_risks
-  #made a dummy variable for ease of reading the code and the math
-  prob_unvaccinated_case = attack_rate_unvaccinated * (1-overall_vaccine_coverage)
-  prob_vaccinated_control = brand_vaccine_coverages * (1- attack_rate_unvaccinated*relative_risks)
-  prob_unvaccinated_control = (1-attack_rate_unvaccinated) * (1-overall_vaccine_coverage)
+  #For each vaccine we know in what proportion are they given in the general population
+  total_vaccine_brands = length(brand_proportions_in_vaccinated)
+  #For each strain we know in what proportion are they affect the total number of cases
+  total_case_strains = length(proportion_strains_in_unvaccinated_cases)
 
-  estimate = matrix(ncol=ncol(relative_VE_combn), nrow = nsims, data = NA)
-  upp = matrix(ncol=ncol(relative_VE_combn), nrow = nsims, data = NA)
-  low = matrix(ncol=ncol(relative_VE_combn), nrow = nsims, data = NA)
-
-  CASE=1
-  CONTROL=2
-  for(j in 1:nsims){
-    cell_counts = c(rmultinom(n=1, size=missing_data_adjusted_total_subjects,
-                              prob = c(prob_vaccinated_case, prob_unvaccinated_case,
-                                       prob_vaccinated_control, prob_unvaccinated_control)))
-    #2 x n table, row 1 for cases, row 2 for controls. nth column for unvaccinated and other columns for different vaccines
-    cell_counts = matrix(cell_counts, nrow=2, byrow = T)
-    #Haldane's correction
-    cell_counts[cell_counts==0] = 0.5
-
-    for(k in 1:ncol(relative_VE_combn)){
-      v1index = relative_VE_combn[1, k]
-      v2index = relative_VE_combn[2, k]
-
-      estimate[j,k] = (cell_counts[CASE, v1index]/sum(cell_counts[, v1index])) / (cell_counts[CASE, v2index]/sum(cell_counts[, v2index]))
-      standard_error = sqrt(
-        cell_counts[CASE, v1index]^-1 +
-          cell_counts[CASE, v2index]^-1 -
-          (cell_counts[CASE, v1index] + cell_counts[CONTROL, v1index])^-1 -
-          (cell_counts[CASE, v2index] + cell_counts[CONTROL, v2index])^-1
-      )
-      confounder_adjusted_standard_error = standard_error / (1-confounder_adjustment_Rsquared)
-      low[j,k] = exp(log(estimate[j,k]) + qnorm(alpha/2) * confounder_adjusted_standard_error)
-      upp[j,k] = exp(log(estimate[j,k]) + qnorm(1 - alpha/2) * confounder_adjusted_standard_error)
-    }
+  relative_VE_combn = matrix(c(1:total_vaccine_brands, rep(total_vaccine_brands+1, total_vaccine_brands)), byrow = T, nrow = 2)
+  if(calculate_relative_VE & total_vaccine_brands>1){
+    relative_VE_combn = cbind(combn(total_vaccine_brands, 2), relative_VE_combn)
   }
+  relative_VE_combn = rbind(relative_VE_combn[,rep(1:ncol(relative_VE_combn), each=total_case_strains)], rep(1:total_case_strains, total_case_strains))
 
-  anticipated_brand_VEs = c(anticipated_brand_VEs, 0)
+  #We are going to create a table with dimensions (row x columns) = (total_case_strains + 1) x (total_vaccine_brands + 1)
+  #The extra 1 column is for unvaccinated
+  #The extra 1 row is for controls
 
-  ret = data.frame(Vaccine1=paste("Vaccine", relative_VE_combn[1,]),
-                   Vaccine2=ifelse(relative_VE_combn[2,]==total_vaccines+1, no = paste("Vaccine", relative_VE_combn[2,]), yes = "Unvaccinated"),
-                   anticipated_VE = apply(relative_VE_combn, 2, FUN = function(x){
-                     1 - (1-anticipated_brand_VEs[x[1]])/(1-anticipated_brand_VEs[x[2]])
-                   }),
-                   expected_VE = apply(1-estimate, MARGIN = 2, FUN = mean, na.rm=T),
-                   avg_lower_limit = apply(1-upp, MARGIN = 2, FUN = mean, na.rm=T),
-                   avg_upper_limit = apply(1-low, MARGIN = 2, FUN = mean, na.rm=T))
+  #the last column of unvaccinated is given by
+  prob_unvaccinated = 1 - overall_vaccine_coverage
+  prob_case_given_unvaccinated = overall_attack_rate_in_unvaccinated
 
-  ret$bias_VE = ret$expected_VE - ret$anticipated_VE
+  #the cell number [total_case_strains + 1, total_vaccine_brands + 1]
+  prob_control_given_unvaccinated = 1 - prob_case_given_unvaccinated
+  prob_control_and_unvaccinated = prob_control_given_unvaccinated * prob_unvaccinated
+
+  #the cell numbers [1:total_case_strains, total_vaccine_brands + 1]
+  #P(strain & case | unvaccinated) p(strain | case, unvaccinated) * p(case|unvaccinated)
+  prob_case_each_strain_given_unvaccinated = proportion_strains_in_unvaccinated_cases * prob_case_given_unvaccinated
+  prob_case_each_strain_and_unvaccinated = prob_case_each_strain_given_unvaccinated * prob_unvaccinated
+
+  #So now we are done with the last column of our table. the remaining columns are for vaccines
+  #sum of columns 1:total_vaccine_brands
+  prob_vaccinated_with_brands = brand_proportions_in_vaccinated * overall_vaccine_coverage
+
+  relative_risk_for_each_brand_and_strain = 1 - do.call('cbind', anticipated_VE_for_each_brand_and_strain)
+  risk_for_each_brand_and_strain_given_unvaccinated = prob_case_each_strain_and_unvaccinated/(prob_case_each_strain_and_unvaccinated + prob_control_and_unvaccinated)
+  risk_for_each_brand_and_strain_given_vaccinated = relative_risk_for_each_brand_and_strain * risk_for_each_brand_and_strain_given_unvaccinated
+  odds_for_each_brand_and_strain_given_vaccinated = (risk_for_each_brand_and_strain_given_vaccinated^-1 - 1)^-1
+  prob_control_each_brand_given_vaccinated = prob_vaccinated_with_brands / (1 + colSums(odds_for_each_brand_and_strain_given_vaccinated))
+  prob_case_each_strain_and_each_brand_given_vaccinated = t(odds_for_each_brand_and_strain_given_vaccinated) * prob_control_each_brand_given_vaccinated
+
+  full_table = cbind(rbind(t(prob_case_each_strain_and_each_brand_given_vaccinated), 'controls'=prob_control_each_brand_given_vaccinated),
+                     'unvaccinated'=c(prob_case_each_strain_and_unvaccinated, prob_control_and_unvaccinated))
+
+  #first row is for vaccine 1, second row is for vaccine 2, third is for strain
+  STRAIN_ROW = 3
+  CONTROL_ROW=ncol(full_table)
+
+  #c(full_table) converts the table into a vector, going column by column
+  cell_counts_sims = array(data = do.call('cbind', lapply(missing_data_adjusted_total_subjects, rmultinom, n=nsims, prob=c(full_table))),
+                           dim = c((total_vaccine_brands+1)*(total_case_strains+1), nsims, total_total_subject_settings))
+
+  #cell_counts_sims = rmultinom(n=nsims, size=missing_data_adjusted_total_subjects, prob = c(full_table))
+  #Haldane's correction
+  cell_counts_sims[cell_counts_sims==0] = 0.5
+
+  #dimensions of the following are ncol(relative_VE_combn) x nsims
+  cases_vaccine1 = cell_counts_sims[(relative_VE_combn[1,]-1)*(total_case_strains +1) + relative_VE_combn[STRAIN_ROW,],,,drop=F]
+  cases_vaccine2 = cell_counts_sims[(relative_VE_combn[2,]-1)*(total_case_strains +1) + relative_VE_combn[STRAIN_ROW,],,,drop=F]
+  controls_vaccine1 = cell_counts_sims[(relative_VE_combn[1,]-1)*(total_case_strains +1) + CONTROL_ROW,,,drop=F]
+  controls_vaccine2 = cell_counts_sims[(relative_VE_combn[2,]-1)*(total_case_strains +1) + CONTROL_ROW,,,drop=F]
+
+  estimate = (cases_vaccine1/(cases_vaccine1 + controls_vaccine1))/(cases_vaccine2/(cases_vaccine2 + controls_vaccine2))
+  standard_error = sqrt(1/cases_vaccine1 + 1/cases_vaccine2 - 1/(cases_vaccine1 + controls_vaccine1) - 1/(cases_vaccine2 + controls_vaccine2))
+  confounder_adjusted_standard_error = standard_error / (1-confounder_adjustment_Rsquared)
+
+  log_estimate = log(estimate)
+  quantile_times_adjusted_standard_error = qnorm(1-alpha/2) * confounder_adjusted_standard_error
+  low = exp(log_estimate - quantile_times_adjusted_standard_error)
+  upp = exp(log_estimate + quantile_times_adjusted_standard_error)
+
+  #expected_VE = rowMeans(1-estimate, na.rm=T)
+  expected_VE = apply(X = 1-estimate, MARGIN = 3, FUN = rowMeans, na.rm=T)
+  avg_lower_limit = apply(X = 1-upp, MARGIN = 3, FUN = rowMeans, na.rm=T)
+  avg_upper_limit = apply(X = 1-low, MARGIN = 3, FUN = rowMeans, na.rm=T)
+
+  relative_risk_for_each_brand_and_strain = cbind(relative_risk_for_each_brand_and_strain,1)
+  relative_risk_vaccine1 = relative_risk_for_each_brand_and_strain[(relative_VE_combn[1,]-1)*total_case_strains + relative_VE_combn[STRAIN_ROW,]]
+  relative_risk_vaccine2 = relative_risk_for_each_brand_and_strain[(relative_VE_combn[2,]-1)*total_case_strains + relative_VE_combn[STRAIN_ROW,]]
+  anticipated_VE = 1 - relative_risk_vaccine1/relative_risk_vaccine2
+
+  ret = data.frame(vaccine_1 = rep(paste("Vaccine", relative_VE_combn[1,]),total_total_subject_settings),
+                   vaccine_2 = rep(ifelse(relative_VE_combn[2,]==total_vaccine_brands+1,
+                                          no = paste("Vaccine", relative_VE_combn[2,]),
+                                          yes = "Unvaccinated"), total_total_subject_settings),
+                   strain = rep(paste("Strain", relative_VE_combn[STRAIN_ROW,]), total_total_subject_settings),
+                   total_subjects = rep(total_subjects, each=ncol(relative_VE_combn)),
+                   anticipated_VE = anticipated_VE,
+                   expected_VE = c(expected_VE),
+                   bias_VE = c(expected_VE - anticipated_VE),
+                   avg_lower_limit = c(avg_lower_limit),
+                   avg_upper_limit = c(avg_upper_limit))
 
   return(ret)
 }
